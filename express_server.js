b0f627915e4f975e768018ad1d7d2b91ce28a24e
stephsteph123 <stephaniepotterrowe@gmail.com>
2022-11-29 15:56:46 -0500

    updates to add url/:id

--- express_server.js
+++ express_server.js
@@ -34,3 +34,8 @@ app.get("/hello", (req, res) => {
   const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
 });
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL:urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
